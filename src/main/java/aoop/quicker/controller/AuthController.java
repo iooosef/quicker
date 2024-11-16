0dbe3008334503f3022afa77a23477092a262983
iooosef <josence22@gmail.com>
2024-11-17 07:22:11 +0800

    Added registration endpoint

--- src/main/java/aoop/quicker/controller/AuthController.java
+++ src/main/java/aoop/quicker/controller/AuthController.java
@@ -1,8 +1,10 @@
 package aoop.quicker.controller;
 
-import aoop.quicker.model.viewmodel.LoginViewModel;
+import aoop.quicker.model.User;
+import aoop.quicker.model.viewmodel.AuthViewModel;
 import aoop.quicker.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,7 +12,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(
             AuthenticationManager authenticationManager,
@@ -34,12 +36,12 @@ public class AuthController {
     }
 
     @RequestMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginViewModel loginViewModel) {
+    public ResponseEntity<String> authenticateUser(@RequestBody AuthViewModel model) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
-                            loginViewModel.getUserName(),
-                            loginViewModel.getUserPassword()
+                            model.getUserName(),
+                            model.getUserPassword()
                     )
             );
 
@@ -51,5 +53,22 @@ public class AuthController {
         } catch (AuthenticationException e) {
             return new ResponseEntity<>("Authentication failed", HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    @RequestMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody AuthViewModel model) {
+        logger.info(model.toString());
+        boolean userExist = userRepository.findByUserName(model.getUserName()).isPresent();
+        if (userExist) {
+            return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setUserName(model.getUserName());
+        user.setUserPassword(passwordEncoder.encode(model.getUserPassword()));
+        user.setUserRoles(model.getUserRoles());
+
+        userRepository.save(user);
+        return new ResponseEntity<>(String.format("User \"%s\" has been registered", user.getUserName()), HttpStatus.CREATED);
     }
 }
