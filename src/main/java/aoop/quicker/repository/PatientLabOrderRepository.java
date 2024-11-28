b550e64830994ff7f724d531799fa3c0b45a19fa
iooosef <josence22@gmail.com>
2024-11-29 03:27:55 +0800

    Changed unused code in PatientLabOrderRepository

--- src/main/java/aoop/quicker/repository/PatientLabOrderRepository.java
+++ src/main/java/aoop/quicker/repository/PatientLabOrderRepository.java
@@ -1,14 +1,8 @@
 package aoop.quicker.repository;
 
 import aoop.quicker.model.PatientLabOrder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Optional;
-
 public interface PatientLabOrderRepository extends JpaRepository<PatientLabOrder, Integer>, JpaSpecificationExecutor<PatientLabOrder> {
-    Page<PatientLabOrder> findAllByAdmissionID(Integer id, Pageable pageable);
-    Optional<PatientLabOrder> findByAdmissionID(Integer id);
 }
