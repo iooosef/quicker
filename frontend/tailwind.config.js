a0737d1f9794f7e3db4768494bafd7c06757fe8d
iooosef <josence22@gmail.com>
2024-12-03 19:32:14 +0800

    Added tailwind and flyonui to frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import flyonui from 'flyonui';
+
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flyonui
+  ],
+}
+
