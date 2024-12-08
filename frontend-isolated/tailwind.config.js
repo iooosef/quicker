7c97bd3433c02ea0c9838a370e44fb02363e1f7b
iooosef <josence22@gmail.com>
2024-12-08 08:05:45 +0800

    Fixed automatic browser dark mode and changed node package version on ts

--- frontend-isolated/tailwind.config.js
+++ frontend-isolated/tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 import flyonui from 'flyonui';
+import plugin from 'flyonui/plugin';
 
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
-  darkMode: false,
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './node_modules/flyonui/dist/js/accordion.js'
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [
-    flyonui
-  ],
+    flyonui,
+    plugin
+  ],flyonui: {
+    themes: ["light"]
+  }
 }
 
